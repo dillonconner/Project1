04e9a1a41d5a6b466e6091d9ca05510195015d74
dillonconner <dillonconner@gmail.com>
2022-08-28 17:18:15 -0400

    changed connection string

--- src/main/java/dev/conner/utils/ConnectionUtil.java
+++ src/main/java/dev/conner/utils/ConnectionUtil.java
@@ -7,11 +7,7 @@ import java.sql.SQLException;
 public class ConnectionUtil {
 
     public static Connection createConnection(){
-        try{
-            
-            Connection conn = DriverManager.getConnection("jdbc:postgresql://conner-sql-server.postgres.database.azure.com:5432/postgres?user=dillonconner&password=Rhodes23$&ssl=false");
-            //
-            //Connection conn = DriverManager.getConnection(System.getenv("AZURE_SQL_DB"));
+        try{Connection conn = DriverManager.getConnection(System.getenv("AZURE_SQL_DB"));
             return conn;
         }catch(SQLException e){
             e.printStackTrace();
