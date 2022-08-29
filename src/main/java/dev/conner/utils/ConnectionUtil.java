0be51731a61bd6f950dfc74871ccd2044d58a767
dillonconner <dillonconner@gmail.com>
2022-08-28 20:51:43 -0400

    altered create connectionutil

--- src/main/java/dev/conner/utils/ConnectionUtil.java
+++ src/main/java/dev/conner/utils/ConnectionUtil.java
@@ -5,15 +5,28 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionUtil {
-
     public static Connection createConnection(){
         try{
-            Connection conn = DriverManager.getConnection(System.getenv("AZURE_SQL_DB"));
+            Connection conn;
+            if(isJUnitTest()){
+                conn = DriverManager.getConnection(System.getenv("AZURE_SQL_DB_TEST"));
+            }else{
+                conn = DriverManager.getConnection(System.getenv("AZURE_SQL_DB"));
+            }
+
             return conn;
         }catch(SQLException e){
             e.printStackTrace();
             return null;
         }
+    }
 
+    public static boolean isJUnitTest() {
+        for (StackTraceElement element : Thread.currentThread().getStackTrace()) {
+            if (element.getClassName().startsWith("org.junit.")) {
+                return true;
+            }
+        }
+        return false;
     }
 }
